6aab3846d46b6b8757d64f0cba7f4c23147b4016
Dory Nehme <doryy.nehme@gmail.com>
2023-06-22 23:11:21 +0300

    Landing Page Done on desktop

--- js/js.js
+++ js/js.js
@@ -11,6 +11,52 @@ $(document).ready(function() {
           $('.navbar-collapse').collapse('hide');
         }
       });
+
+
+
+
+      /**Animation */
+      function PinkSeperatorAnim() {
+        var reveals = document.querySelectorAll(".PinkSeperator, .RoundedImageCarousel ,.Cards3Part,.ProductCarouse,.whyUs,.youNeedHelp,.FooterCards,.FooterServices ,.AnimatedRow");
+      
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+      
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
+          }
+        }
+      }
+      
+      window.addEventListener("scroll", PinkSeperatorAnim); 
+      
+      
+
+      $(window).scroll(function() {
+        var targetDiv = $('.col-xl-8  .d-flex.flex-column.justify-content-between.align-items-center');
+        var targetImage = targetDiv.find('img');
+      
+        var windowHeight = $(window).height();
+        var scrollPosition = $(window).scrollTop();
+        var targetDivPosition = targetDiv.offset().top;
+        var targetDivHeight = targetDiv.outerHeight();
+      
+        if (scrollPosition > targetDivPosition - windowHeight + 200 &&
+            scrollPosition < targetDivPosition + targetDivHeight) {
+          targetImage.addClass('animate__animated animate__slideInLeft  ');
+          targetImage.removeClass('animate__slideOutLeft d-none');
+        } else {
+          targetImage.removeClass('animate__slideInLeft');
+          targetImage.addClass('animate__slideOutLeft d-none');
+        }
+      });
+      
+
+
   });
 
 
